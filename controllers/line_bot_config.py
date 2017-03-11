f4a7ed7cf56ab07d3e50c40bb9d4c63dfac3ecf8
cwen0708 <cwen0708@gmail.com>
2017-03-12 05:20:49 +0800

    設定頁面

---
+++ controllers/line_bot_config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created with YooLiang Technology (侑良科技).
+# Author: Qi-Liang Wen (温啓良）
+# Web: http://www.yooliang.com/
+# Date: 2017/2/23.
+
+from argeweb import Controller, scaffold, route_menu, Fields, route_with, route
+from argeweb.components.pagination import Pagination
+from argeweb.components.search import Search
+
+
+class LineBotConfig(Controller):
+    class Meta:
+        components = (scaffold.Scaffolding, Pagination, Search)
+
+    @route
+    @route_menu(list_name=u'backend', text=u'Line Bot 設定', sort=801, group=u'互動項目')
+    def admin_config(self):
+        record = self.meta.Model.find_by_name(self.namespace)
+        if record is None:
+            record = self.meta.Model()
+            record.name = self.namespace
+            record.put()
+        return scaffold.edit(self, record.key)
+
